8dbd8b6a1b6ae3b6dce25efd4b69879933fb7cb8
WHD <e19880910@gmail.com>
2017-06-20 22:59:38 +0800

    產生第一個測試案例，測試結果為紅燈。

---
+++ HarryPotterCartDLL/HarryPotterCartDLL.Test/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HarryPotterCartDLL.Test
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void Buy1Book_Return_100()
+		{
+			//arrange
+			var target = new PotterShoppingCart();
+			target.Buy(1, 1);
+			target.Buy(2, 0);
+			target.Buy(3, 0);
+			target.Buy(4, 0);
+			target.Buy(5, 0);
+			
+			//act
+			var actual = target.CheckOut();
+			var expected = 100;
+
+			//assert
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
---
+++ HarryPotterCartDLL/HarryPotterCartDLL/PotterShoppingCart.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HarryPotterCartDLL
+{
+	public class PotterShoppingCart
+	{
+		public PotterShoppingCart()
+		{
+		}
+
+		public void Buy(int v1, int v2)
+		{
+			throw new NotImplementedException();
+		}
+
+		public object CheckOut()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
